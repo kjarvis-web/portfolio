e23cbddfb90b4a851d5f49d33ddfa39cc322886e
norristown <jarvasity@gmail.com>
2023-11-24 20:45:32 -0500

    project section

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,5 +1,47 @@
-function ProjectCard({ imgUrl, title, description }) {
-  return <div></div>;
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+function ProjectCard({ imgUrl, title, description, gitUrl, previewUrl }) {
+  return (
+    <div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{
+          background: `url(${imgUrl})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="overlay flex justify-center items-center gap-8 absolute top-0 left-0 w-full h-full bg-zinc-900 bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 rounded-t-xl">
+          <div className="mr-2 flex flex-col items-center group/link">
+            <Link
+              href={gitUrl}
+              className="border-2 flex justify-center items-center rounded-full border-zinc-400 group-hover/link:border-white"
+            >
+              <CodeBracketIcon className="text-zinc-400 h-10 w-10 m-2 cursor-pointer group-hover/link:text-white" />
+            </Link>
+            <p className="text-zinc-400 mt-1 group-hover/link:text-white">
+              Code
+            </p>
+          </div>
+          <div className="flex flex-col items-center group/link">
+            <Link
+              href={previewUrl}
+              className="border-2 flex justify-center items-center rounded-full border-zinc-400 group-hover/link:border-white"
+            >
+              <EyeIcon className="text-zinc-400 h-10 w-10 m-2 cursor-pointer group-hover/link:text-white" />
+            </Link>
+            <p className="text-zinc-400 mt-1 group-hover/link:text-white">
+              Preview
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-b-xl bg-zinc-900 text-zinc-100 py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="">{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,5 +1,49 @@
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Project 1 title",
+    description: "Project 1 description",
+    image: "/images/computer.jpg",
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Project 2 title",
+    description: "Project 2 description",
+    image: "/images/computer.jpg",
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Project 3 title",
+    description: "Project 3 description",
+    image: "/images/computer.jpg",
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
 function ProjectSection() {
-  return <h2>My Projects</h2>;
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold mt-4 mb-4">My Projects</h2>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default ProjectSection;
