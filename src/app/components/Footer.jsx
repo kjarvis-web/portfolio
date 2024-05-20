e1ddc44d8bc2291399e250c119ec9242be2cff2c
norristown <jarvasity@gmail.com>
2024-05-20 02:34:13 -0400

    updated footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,8 +1,7 @@
 function Footer() {
   return (
-    <footer className="footer bg-zinc-900 text-zinc-400 flex justify-between p-6">
+    <footer className="footer bg-zinc-900 text-zinc-400 flex justify-center p-6">
       <p>Kevin Jarvis</p>
-      <p>&copy; All Rights Reserved</p>
     </footer>
   );
 }
